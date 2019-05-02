8f8e3e28287931612ad0cac01cf080b693035f4d
yumetodo <yume-wikijp@live.jp>
2019-05-03 02:42:09 +0900

    fix(test): add more test

--- test/test.js
+++ test/test.js
@@ -5,11 +5,20 @@ const GraphemeBreaker = require('grapheme-breaker-u10-0');
 describe('test', () => {
   it('count_condepoint_len', () => {
     expect(countCodepointLen('a')).toEqual(1);
+    expect(countCodepointLen('が')).toEqual(1);
+    expect(countCodepointLen('\u304b\u3099')).toEqual(2);
+    expect(countCodepointLen('👩‍❤️‍💋‍👨')).toEqual(8);
   });
   it('GraphemeBreaker.countBreaks(s)', () => {
     expect(GraphemeBreaker.countBreaks('a')).toEqual(1);
+    expect(GraphemeBreaker.countBreaks('が')).toEqual(1);
+    expect(GraphemeBreaker.countBreaks('\u304b\u3099')).toEqual(1);
+    expect(GraphemeBreaker.countBreaks('👩‍❤️‍💋‍👨')).toEqual(1);
   });
   it('t', () => {
     expect(hasCombiningCharacter('a')).toEqual(false);
+    expect(hasCombiningCharacter('が')).toEqual(false);
+    expect(hasCombiningCharacter('\u304b\u3099')).toEqual(true);
+    expect(hasCombiningCharacter('👩‍❤️‍💋‍👨')).toEqual(true);
   });
 });
